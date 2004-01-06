b008641759faa55167169dd8445b1f07abe9f385
danny <danny>
2004-01-06 11:24:01 +0000

    Tastatursteuerung überarbeitet und auskommentiert.

--- aigine/src/AiGinEFramework/Control/Keyboard.cpp
+++ aigine/src/AiGinEFramework/Control/Keyboard.cpp
@@ -1,78 +1,19 @@
 /* Game Engine Design */
 
 #include "Keyboard.h"
+#include "../GraphicEngine/SceneManagement.h"
 
-
-
+//____________________________________________________________________________
 void Keyboard::keyAction(unsigned char key, int mouseX, int mouseY){
-   switch (key) {
-      case ('w'): // move front
-		  //this->sceneMan->getCamera()->moveCamera(0.5);
-      this->sceneMan->getCamera()->moveForward(0.5);
-         glutPostRedisplay();
-         break;
-      case ('s'): // nach back
-		  //this->sceneMan->getCamera()->moveCamera(-0.5);
-        this->sceneMan->getCamera()->moveBack(-0.5);
-         glutPostRedisplay();
-         break;
-      case ('a'): // move left
-		  //this->sceneMan->getCamera()->strafeCamera(-0.5)
-        this->sceneMan->getCamera()->moveLeft(-0.5);
-         glutPostRedisplay();
-         break;
-      case ('d'): // move right
-		  //this->sceneMan->getCamera()->strafeCamera(0.5); 
-        this->sceneMan->getCamera()->moveRight(0.5);
-         glutPostRedisplay();
-         break;
-      case 27: // ESC key code
-         exit(0);
-         break; 
-
-/*      case ('y'):
-         //_y_angular++;
-         _active_angle->y++;
-         glutPostRedisplay();
-         break;
-      case ('z'):
-         _active_angle->z++;
-         glutPostRedisplay();
-         break;
-      case ('X'):
-         //_x_angular--;
-         _active_angle->x--;
-         glutPostRedisplay();
-         break;
-      case ('Y'):
-         //_y_angular--;
-         _active_angle->y--;
-         glutPostRedisplay();
-         break;
-      case ('Z'):
-         _active_angle->z--;
-         //_z_angular--;
-         glutPostRedisplay();
-         break;
-      case('0'):
-         _active_angle = &_arm_angles[0];
-         break;
-      case('1'):
-         _active_angle = &_arm_angles[1];
-         break;
-      case('2'):
-         _active_angle = &_arm_angles[2];
-         break;
-      case('3'):
-         _active_angle = &_arm_angles[3];
-         break;
-*/
-
-   }  
-
+	if(key ==  27) exit(0); // ESC for program quit
+	if(key == 'w') this->sceneMan->getCamera()->moveForward(0.5);
+	if(key == 's') this->sceneMan->getCamera()->moveBack(-0.5);
+	if(key == 'a') this->sceneMan->getCamera()->moveLeft(-0.5);
+	if(key == 'd') this->sceneMan->getCamera()->moveRight(0.5);
+	glutPostRedisplay();
 }
 
-
+//____________________________________________________________________________
 Keyboard::Keyboard(SceneManagement* sceneMan)
 {
 	this->sceneMan = sceneMan;
--- aigine/src/AiGinEFramework/Control/Keyboard.h
+++ aigine/src/AiGinEFramework/Control/Keyboard.h
@@ -4,21 +4,41 @@
 #define KEYBOARD_H
 
 #include <GL/glut.h>
-//#include <iostream>
-//using namespace std;
 
-#include "../GraphicEngine/SceneManagement.h"
-#include "../Model/Point3D.h"
-
-//class SceneManagement;
+class SceneManagement;
 class Point3D;
 
+
+/**
+ * "Keyboard" handelt die zugewiesenen Tastaturkommandos und für
+ * die entsprechenden Methoden auf dem "SceneManagment" aus.
+ *
+ * TODO:
+ * - reagiert derzeitig nur auf einzelne Tasten, schön wäre natürlich
+ *   wenn man in meherere Richtungen (z.B. nach vorne links) laufen könnte.
+ *
+ * @author Danny Graef, Tobias Harpering
+ * @date 2004-01-06
+ */
 class Keyboard
 {
 public:
+	/**
+	 * Konstruktor	 
+	 */
 	Keyboard(SceneManagement* sceneMan);
 
+    /**
+     * Funktion für OpenGL keyboard() Pointer.
+     * Prüft welche Taste gedrückt wurde und für die zugewiesene
+	 * Methode auf.
+     */
     void keyAction(unsigned char key, int x, int y);    
+
+private:
+	/**
+	 * Zugewiesenes "SceneManagment".
+	 */
 	SceneManagement* sceneMan;
 };
 #endif //KEYBOARD_H
