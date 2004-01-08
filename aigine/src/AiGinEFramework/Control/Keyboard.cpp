274a0fb536fd200d03f432682c71ffb36e62db46
danny <danny>
2004-01-08 02:58:54 +0000

    Umstellung auf geändertes Camera Objekt.

--- aigine/src/AiGinEFramework/Control/Keyboard.cpp
+++ aigine/src/AiGinEFramework/Control/Keyboard.cpp
@@ -1,4 +1,6 @@
-/* Game Engine Design */
+/* Game Engine Design 
+ * Keyboard.cpp  
+ */
 
 #include "Keyboard.h"
 #include "../GraphicEngine/SceneManagement.h"
@@ -7,8 +9,8 @@
 void Keyboard::keyAction(unsigned char key, int mouseX, int mouseY){
 	if(key ==  27) exit(0); // ESC for program quit
 	if(key == 'w') this->sceneMan->getCamera()->moveForward(0.5);
-	if(key == 's') this->sceneMan->getCamera()->moveBack(-0.5);
-	if(key == 'a') this->sceneMan->getCamera()->moveLeft(-0.5);
+	if(key == 's') this->sceneMan->getCamera()->moveBack(0.5);
+	if(key == 'a') this->sceneMan->getCamera()->moveLeft(0.5);
 	if(key == 'd') this->sceneMan->getCamera()->moveRight(0.5);
 	glutPostRedisplay();
 }
