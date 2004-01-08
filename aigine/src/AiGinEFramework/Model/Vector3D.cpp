9d26caa2d347681e77676a28c3edff03103fa9c9
danny <danny>
2004-01-08 03:06:22 +0000

    Point3D wurde durch Vector3D ersetzt. Vectoroperationen wurden in diesem Objekt zusammengeführt - jetzt müssten nur noch die Loader etc. darauf zugreifen anstatt ihre eigenen zu verwenden.

--- aigine/src/AiGinEFramework/Model/Vector3D.cpp
+++ aigine/src/AiGinEFramework/Model/Vector3D.cpp
@@ -1,10 +1,93 @@
-/* Game Engine Design */
+/* Game Engine Design 
+ * Vector3D.cpp  
+ */
 
 #include "Vector3D.h"
-#include <iostream.h>
+#include <iostream>
+#include <math.h>
 
-Vector3D::Vector3D(float x, float y, float z) : Point3D(x, y, z){
-	print();
+using namespace std;
+
+//____________________________________________________________________________
+Vector3D::Vector3D(float x, float y, float z){
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+//____________________________________________________________________________
+Vector3D Vector3D::operator+(Vector3D vVector)
+{
+	return Vector3D(vVector.x + this->x, vVector.y + this->y, vVector.z + this->z);
+}
+
+//____________________________________________________________________________
+Vector3D  Vector3D::operator-(Vector3D vVector)
+{
+	return Vector3D(this->x - vVector.x, this->y - vVector.y, this->z - vVector.z);
+}
+	
+//____________________________________________________________________________
+Vector3D  Vector3D::operator*(float num)
+{
+	return Vector3D(this->x * num, this->y * num, this->z * num);
+}
+
+//____________________________________________________________________________
+Vector3D  Vector3D::operator/(float num)
+{
+	return Vector3D(this->x / num, this->y / num, this->z / num);
+}
+
+//____________________________________________________________________________
+Vector3D Vector3D::cross(Vector3D * vector1, Vector3D * vector2)
+{
+  Vector3D temp = Vector3D(0.0, 0.0, 0.0);
+  // bilde Vektorprodukt durch Berechnung der einzelnen Komponenten
+  temp.x = ((vector1->y * vector2->z) - (vector1->z * vector2->y));
+  temp.y = ((vector1->z * vector2->x) - (vector1->x * vector2->z));
+  temp.z = ((vector1->x * vector2->y) - (vector1->y * vector2->x));
+  return temp;
+}
+
+//____________________________________________________________________________
+Vector3D Vector3D::getNormalVector(Vector3D vector)
+{
+    // Get the magnitude of our normal
+    float magn = magnitude(vector);
+
+    // Now that we have the magnitude, we can divide our vector by that magnitude.
+    // That will make our vector a total length of 1.
+    vector = vector / magn;
+
+    // Finally, return our normalized vector
+    return vector;
+}
+
+//____________________________________________________________________________
+float Vector3D::magnitude(Vector3D vector)
+{
+    // Here is the equation:  magnitude = sqrt(V.x^2 + V.y^2 + V.z^2) : Where V is the vector
+    return (float)sqrt((vector.x * vector.x) + (vector.y * vector.y) +
+       (vector.z * vector.z));
 }
 
 
+//____________________________________________________________________________
+void Vector3D::print(){
+	cout << "x: " << this->x << "   y: " << this->y << "   z: " << this->z  << endl;
+}
+
+//____________________________________________________________________________
+void Vector3D::print(char* description){
+	cout << description;
+	print();
+}
+
+//____________________________________________________________________________
+Vector3D::Vector3D()
+{
+  this->x = 0.0;
+  this->y = 0.0;
+  this->z = 0.0;
+}
--- aigine/src/AiGinEFramework/Model/Vector3D.h
+++ aigine/src/AiGinEFramework/Model/Vector3D.h
@@ -1,14 +1,113 @@
-/* Game Engine Design */
+/* Game Engine Design 
+ * Vector3D.h  
+ */
 
 #ifndef VECTOR3D_H
 #define VECTOR3D_H
-#include "Point3D.h"
-class Vector3D : public Point3D
-{
-public: 
-	Vector3D(float x, float y, float z) ;
+
+/**
+ * Bechreibung eines Vektors und statische Implemtierung
+ * der Vektormathematik.
+ * @author Danny Gräf
+ * @since 2004-01-08
+ */
+class Vector3D {
+public:
+
+  /**
+   * Konstruktor, initialisiert alle Koordinaten mit 0.0.
+   */
+	Vector3D();
+
+  /**
+   * Konstruktor, initialisiert alle Koordinaten mit den
+   * übergebenen Werten.
+   * @param x Koordinate.
+   * @param y Koordinate.
+   * @param z Koordinate.
+   */
+    Vector3D(float x, float y, float z);
+
+  /**
+   * Vektoraddition.
+   * @param vector Summand
+   * @return Summe beider Vektoren.
+   */
+	Vector3D operator+(Vector3D vector);
+
+  /**
+   * Vektorsubtraktion
+   * @param vector Subtrahend.
+   * @return Differenz beider Vektoren.
+   */
+  Vector3D operator-(Vector3D vector);
+
+  /**
+   * Multiplikation von Vektor und reeler Zahl.
+   * @param num reele Zahl.
+   * @return Produkt.
+   */
+	Vector3D operator*(float num);
+
+  /**
+   * Division von Vektor und reeler Zahl.
+   * @param Divisor.
+   * @return Quotient.
+   */
+  Vector3D operator/(float num);
+
+    /**
+     * Gibt das Kreuzprodukt (den senkrecht stehenden Vektor) 
+     * der beiden Vektoren zurück.
+     * @param vector1 erster Vektor
+     * @param vector2 zweiter Vektor
+     * @return Kreuzprodukt
+     */
+  static Vector3D cross(Vector3D * vector1, Vector3D * vector2);
+
+  /**
+   * Gibt den normalisierten Vektor zurück.
+   * @param vector der normalisiert werden soll.
+   * @return Normalisierter Vektor.
+   */  
+  static Vector3D getNormalVector(Vector3D vector);
+
+  /**
+   * Gibt die Länge eines Vektors zurück.
+   * @param vector dessen Länge berechnet werden soll.
+   * @return Länge des Vektors.
+   */  
+  static float magnitude(Vector3D vector);
+  
+  /**
+   * Gibt die Beschreibung und die Vektorkoordinaten 
+   * in der Konsole aus.
+   * @param description Beschreibung des Vektors.
+   */
+	void print(char* description);
+
+  /**
+   * x Koordinate des Vektors
+   */
+    float z;
+
+  /**
+   * y Koordinate des Vektors
+   */
+    float y;
+
+  /**
+   * z Koordinate des Vektors
+   */
+    float x;
 
 
+private:
 
+
+  /**
+   * Gibt die Vektorkoordinaten in der Konsole aus.
+   */
+  void print();
 };
 #endif //VECTOR3D_H
