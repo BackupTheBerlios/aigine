f56d82c6631a59f7b98ed6c2d4cbead4071a763a
fosion <fosion>
2004-01-14 15:47:07 +0000

    Klasse für speicherung von Skalierung abgeleitet von Vector3D

---
+++ aigine/src/AiGinEFramework/Model/Scale3D.h
@@ -0,0 +1,14 @@
+/* Game Engine Design */
+
+#ifndef SCALE3D_H
+#define SCALE3D_H
+#include "Vector3D.h"
+
+/**
+ * Skalierung eines 3DOBJ
+ * @author Frank Otto 
+ */
+class Scale3D : public Vector3D
+{
+};
+#endif //SCALE3D_H
