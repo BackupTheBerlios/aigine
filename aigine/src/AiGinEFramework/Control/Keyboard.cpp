3d67911eb9b851edeb657d7639b1212fc4338b84
danny <danny>
2004-01-22 19:50:26 +0000

    fosion hat nebelaufruf eingebaut

--- aigine/src/AiGinEFramework/Control/Keyboard.cpp
+++ aigine/src/AiGinEFramework/Control/Keyboard.cpp
@@ -1,5 +1,5 @@
-/* Game Engine Design 
- * Keyboard.cpp  
+/* Game Engine Design
+ * Keyboard.cpp
  */
 
 #include "Keyboard.h"
@@ -14,7 +14,7 @@ void Keyboard::keyAction(unsigned char key, int mouseX, int mouseY){
 	if(key == 'd') this->sceneMan->getCamera()->moveRight(0.5);
 	if(key == 'n') this->sceneMan->getCamera()->setViewFogMode(true,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);//spaeter ueber einstellungsdialog bzw inidatei
     if(key == 'N') this->sceneMan->getCamera()->setViewFogMode(false,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);
-	
+
 //	glutPostRedisplay();
 }
 
