585440245079fd5b5dbe69ec8ecdaed4b6d1876f
fosion <fosion>
2004-01-22 19:40:12 +0000

    n N schaltet den Nebel ein aus

--- aigine/src/AiGinEFramework/Control/Keyboard.cpp
+++ aigine/src/AiGinEFramework/Control/Keyboard.cpp
@@ -12,6 +12,9 @@ void Keyboard::keyAction(unsigned char key, int mouseX, int mouseY){
 	if(key == 's') this->sceneMan->getCamera()->moveBack(0.5);
 	if(key == 'a') this->sceneMan->getCamera()->moveLeft(0.5);
 	if(key == 'd') this->sceneMan->getCamera()->moveRight(0.5);
+	if(key == 'n') this->sceneMan->getCamera()->setViewFogMode(true,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);//spaeter ueber einstellungsdialog bzw inidatei
+    if(key == 'N') this->sceneMan->getCamera()->setViewFogMode(false,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);
+	
 //	glutPostRedisplay();
 }
 
