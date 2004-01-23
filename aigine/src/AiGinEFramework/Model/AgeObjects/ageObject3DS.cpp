e7ae41f14857afcc8b01adc862a8bf3b4283c17b
fosion <fosion>
2004-01-23 18:30:31 +0000

    den `chrome` mapping abgeschaltet

--- aigine/src/AiGinEFramework/Model/AgeObjects/ageObject3DS.cpp
+++ aigine/src/AiGinEFramework/Model/AgeObjects/ageObject3DS.cpp
@@ -16,10 +16,10 @@ ageObject3DS::ageObject3DS(string fileName): AiGinEObject() {
 	this->my3DSObject->loadObject((char*)fileName.c_str());
 //	this->my3DSObject->model.pObject.
 	glBindTexture(GL_TEXTURE_2D, this->my3DSObject->getTextureID(0));
-	glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-	glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-	glEnable(GL_TEXTURE_GEN_S);     // Enable spherical
-	glEnable(GL_TEXTURE_GEN_T);     // Environment Mapping
+//	glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+//	glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+//	glEnable(GL_TEXTURE_GEN_S);     // Enable spherical
+//	glEnable(GL_TEXTURE_GEN_T);     // Environment Mapping
 }
 
 ageObject3DS::~ageObject3DS()
