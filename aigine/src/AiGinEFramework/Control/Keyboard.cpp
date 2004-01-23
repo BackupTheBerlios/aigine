8a4639dd5641522c7595ffd8b560fa23af558a93
fosion <fosion>
2004-01-23 18:58:56 +0000

    schrittweite an Kamera angepasst

--- aigine/src/AiGinEFramework/Control/Keyboard.cpp
+++ aigine/src/AiGinEFramework/Control/Keyboard.cpp
@@ -8,12 +8,12 @@
 //____________________________________________________________________________
 void Keyboard::keyAction(unsigned char key, int mouseX, int mouseY){
 	if(key ==  27) exit(0); // ESC for program quit
-	if(key == 'w') this->sceneMan->getCamera()->moveForward(0.5);
-	if(key == 's') this->sceneMan->getCamera()->moveBack(0.5);
-	if(key == 'a') this->sceneMan->getCamera()->moveLeft(0.5);
-	if(key == 'd') this->sceneMan->getCamera()->moveRight(0.5);
-	if(key == 'n') this->sceneMan->getCamera()->setViewFogMode(true,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);//spaeter ueber einstellungsdialog bzw inidatei
-    if(key == 'N') this->sceneMan->getCamera()->setViewFogMode(false,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 60.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);
+	if(key == 'w') this->sceneMan->getCamera()->moveForward(1.0);
+	if(key == 's') this->sceneMan->getCamera()->moveBack(1.0);
+	if(key == 'a') this->sceneMan->getCamera()->moveLeft(1.0);
+	if(key == 'd') this->sceneMan->getCamera()->moveRight(1.0);
+	if(key == 'n') this->sceneMan->getCamera()->setViewFogMode(true,254.0, 236.0, 208.0, 1.0, 0.2, 100.0, 160.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);//spaeter ueber einstellungsdialog bzw inidatei
+    if(key == 'N') this->sceneMan->getCamera()->setViewFogMode(false,0.0, 0.0, 0.0, 1.0, 0.2, 30.0, 100.0, GL_LINEAR,GL_FOG_HINT, GL_DONT_CARE);
 
 //	glutPostRedisplay();
 }
