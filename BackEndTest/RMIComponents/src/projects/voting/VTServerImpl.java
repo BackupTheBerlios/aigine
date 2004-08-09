f6a646be00a9e5c659c3b712e07343da03e7dd6c
danny <danny>
2004-08-09 22:15:19 +0000

    anpassungen an neue Serverstruktur.

--- BackEndTest/RMIComponents/src/projects/voting/VTServerImpl.java
+++ BackEndTest/RMIComponents/src/projects/voting/VTServerImpl.java
@@ -16,81 +16,93 @@ import API.model.RemoteObject;
  * @author danny
  * @since 05.05.2004 20:37:39
  */
-public class VTServerImpl
-  extends Server
-	implements VTServer {
-		
-	/**
-	 * die Votes
-	 */
-	private VoteTable votes = null;
+public class VTServerImpl extends Server implements VTServer {
 
-	/** 
-	 * Start des Servers und einlesen der Votes.
-	 * @throws <{RemoteException}>
-	 */
-	public VTServerImpl() throws RemoteException {
-		System.out.println("=> VTServerImpl.VTServerImpl()");
-		try {
-			votes = new VoteTable("votes.dat");
-		} catch (IOException e) {  
-			e.printStackTrace();
-			System.out.println(
-				"Fehler in VTServerImpl.VTServerImpl() > " + e.getMessage());
-		}
-		System.out.println("<= VTServerImpl.VTServerImpl()");
-	}
+    /**
+     * die Votes
+     */
+    private VoteTable votes= null;
 
-	/**
-	 * Update aller registrierten Clients. Wenn ein Client nicht 
-	 * erreichbar ist, wird er aus der Liste entfernt.
-	 */
-	public synchronized void updateClients() {
-		System.out.println("=> VTServerImpl.updateClients()");
-		Enumeration elem = remoteObjects.elements();
-		while (elem.hasMoreElements()) {
-			RemoteObject remoteObject = (RemoteObject) elem.nextElement();
-			VTClient client = (VTClient)remoteObject.getApp();
-			try {
-				System.out.println("try update \n\t" + client + "\n");
-				client.update(votes);				
-			} catch (RemoteException e) {
-				e.printStackTrace();
-				System.out.println(
-					"Fehler in VTServerImpl.updateClients() " +
-					"projects.projects.Client update fehlgeschlagen:\n\t" + client + " > " + e.getMessage());
-				remoteObjects.remove(client);
-			}
-		}
-		System.out.println("<= VTServerImpl.updateClients()");
-	}
-	
-	/**
-	 * Registrierung des Clients. Ruft auf dem Client die update() 
-	 * Methode auf und übergibt im somit die derzeitigen Daten.
-	 * @param projects.projects.Client - der registriert werden soll.
-	 * @throws <{RemoteException}>
-	*/
-	public synchronized String register(RemoteObject remoteObject)
-		throws RemoteException {
-		System.out.println("=> VTServerImpl.register()");
-		String result = super.register(remoteObject);
-		VTClient app = (VTClient)remoteObject.getApp();
-		app.update(votes);
-		System.out.println("Status " + result +  " | client: " + app );
-		System.out.println("<= VTServerImpl.register()");
-		return result;
-	}
+    /** 
+     * Start des Servers und einlesen der Votes.
+     * @throws <{RemoteException}>
+     */
+    public VTServerImpl() throws RemoteException {
+        System.out.println("=> VTServerImpl.VTServerImpl()");
+        try {
+            votes= new VoteTable("votes.dat");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(
+                "Fehler in VTServerImpl.VTServerImpl() > " + e.getMessage());
+        }
+        System.out.println("<= VTServerImpl.VTServerImpl()");
+    }
 
-	/**
-	 * Abgabe eines Votes.
-	 * @param voteid - des gewählten Votes.
-	 */
-	public synchronized void vote(String voteid) throws RemoteException {
-		if(votes.containsKey(voteid)) {
-			Vote vote = (Vote)votes.get(voteid);
-			vote.setCount(vote.getCount() + 1);
-		}
-		this.updateClients();
-	}
+    /**
+     * Update aller registrierten Clients. Wenn ein Client nicht 
+     * erreichbar ist, wird er aus der Liste entfernt.
+     */
+    public synchronized void updateClients() {
+        System.out.println("=> VTServerImpl.updateClients()");
+        Enumeration elem= remoteObjects.elements();
+        while (elem.hasMoreElements()) {
+            VTClient client = null;
+            RemoteObject remoteObject;
+            try {
+                remoteObject= (RemoteObject) elem.nextElement();
+                client= (VTClient) remoteObject.getApp();
+                System.out.println("try update \n\t" + client + "\n");
+                client.update(votes);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                System.out.println(
+                    "Fehler in VTServerImpl.updateClients() "
+                        + "update fehlgeschlagen:\n\t"
+                        + client
+                        + " > "
+                        + e.getMessage());
+                remoteObjects.remove(client);
+            } catch (ClassCastException e) {
+                e.printStackTrace();
+                System.out.println(
+                    "Fehler in VTServerImpl.updateClients() "
+                        + "update fehlgeschlagen:\n\t"
+                        + client
+                        + " > "
+                        + e.getMessage());
+                remoteObjects.remove(client);
+            }
+        }
+        System.out.println("<= VTServerImpl.updateClients()");
+    }
+
+    /**
+     * Registrierung des Clients. Ruft auf dem Client die update() 
+     * Methode auf und übergibt im somit die derzeitigen Daten.
+     * @param projects.projects.Client - der registriert werden soll.
+     * @throws <{RemoteException}>
+    */
+    public synchronized String register(RemoteObject remoteObject)
+        throws RemoteException {
+        System.out.println("=> VTServerImpl.register()");
+        String result= super.registerComponent(remoteObject);
+        VTClient app= (VTClient) remoteObject.getApp();
+        app.update(votes);
+        System.out.println("Status " + result + " | client: " + app);
+        System.out.println("<= VTServerImpl.register()");
+        return result;
+    }
+
+    /**
+     * Abgabe eines Votes.
+     * @param voteid - des gewählten Votes.
+     */
+    public synchronized void vote(String voteid) throws RemoteException {
+        if (votes.containsKey(voteid)) {
+            Vote vote= (Vote) votes.get(voteid);
+            vote.setCount(vote.getCount() + 1);
+        }
+        this.updateClients();
+    }
 }
