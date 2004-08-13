2d3d94f24442d81b143ed06ee9cfb08b470cb0a6
fosion <fosion>
2004-08-13 16:02:04 +0000

    Helperklasse welche remoteobjekte aus konfigurationen (JConfig) erzeugt

---
+++ BackEndTest/RMIComponents/src/API/util/RemoteObjectHelper.java
@@ -0,0 +1,53 @@
+package API.util;
+import org.jconfig.Configuration;
+import API.model.RemoteObject;
+/**
+*
+* Utility Klasse welche aus uebergebener 
+* JConfig Configuration+gewuenschter category 
+* alle Einstellungen in ein remoteObject packt und dieses
+* zurueck gibt 
+* @author Frank Otto s0503712
+*/
+public class RemoteObjectHelper {
+
+	private RemoteObject ro;
+	
+  /**
+   * Instanziert ein remoteObject und fuettert es mit den Konfigurationsdaten
+   * -->trennung model/util
+	 * @param configuration JConfig Configuration
+	 * @param category Kategorie der _config.xml Datei 
+	 */
+  
+  //TODO  XML Properties Schemaprüfung
+	public RemoteObjectHelper(Configuration configuration, String category) {
+
+		ro = new RemoteObject();
+		System.out.println("=> RemoteObjectProperties()");
+
+		ro.setDescription(configuration.getProperty("description", "", category));
+		ro.setServicetyp(configuration.getProperty("typ", "", category));
+		ro.setCodebase(configuration.getProperty("codebase", "", category));
+		ro.setCompClassName(configuration.getProperty("compClassName", "", category));
+		ro.setCompName(configuration.getProperty("compName", "", category));
+		ro.setPort(configuration.getProperty("port", "", category));
+		ro.setHostname(configuration.getProperty("hostname", "", category));
+		ro.setAuthTyp(configuration.getProperty("authTyp", "", category));
+		ro.setUsername(configuration.getProperty("user", "", category));
+		ro.setPasswd(configuration.getProperty("passwd", "", category));
+		ro.setRmiName("rmi://" + ro.getHostname() + ":" + ro.getPort() + "/");
+		
+    System.out.println("<= RemoteObjectProperties() > " + this);
+
+	}
+  /**
+   * 
+	 * @return Konfiguriertes RemoteObject
+	 */
+	public RemoteObject getConfiguratedRemoteObject(){
+    return ro;
+  }
+
+
+}
