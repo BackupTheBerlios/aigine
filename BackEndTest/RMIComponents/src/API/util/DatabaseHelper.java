6e1428c0ffbdf9d0688872897701e5416752a7bb
mr_nice <mr_nice>
2004-08-15 15:57:09 +0000

    neue propertie für die service funktionalitaet

---
+++ BackEndTest/RMIComponents/src/API/util/DatabaseHelper.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 15.08.2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package API.util;
+
+/**
+ * @author tobi
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class DatabaseHelper   {
+	
+	public DatabaseHelper() {
+		
+	}
+
+}
