bd346646e837ff2b43af29a4a9370aff84e3db68
coda <coda>
2005-11-07 12:37:01 +0000

    neu

---
+++ AGE/TriBase/Res/Resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define TB_VS_VERSION_INFO              1
+#define TB_IDD_CONFIG                   101
+#define TB_IDB_BITMAP1                  102
+#define TB_IDI_ICON1                    103
+#define TB_IDC_C_OK                     1001
+#define TB_IDC_C_CANCEL                 1002
+#define TB_IDC_D3DC_ADAPTERS            1003
+#define TB_IDC_D3DC_DEVICE_TYPES        1004
+#define TB_IDC_D3DC_WINDOWED_MODE       1005
+#define TB_IDC_D3DC_FULLSCREEN_MODE     1006
+#define TB_IDC_D3DC_VIDEO_MODES         1007
+#define TB_IDC_D3DC_BACK_BUFFER_FORMATS 1008
+#define TB_IDC_D3DC_Z_STENCIL_BUFFER_FORMATS 1009
+#define TB_IDC_D3DC_MULTI_SAMPLING_TYPES 1010
+#define TB_IDC_D3DC_MULTI_SAMPLING_QUALITIES 1011
+#define TB_IDC_D3DC_HARDWARE_TNL        1012
+#define TB_IDC_D3DC_SOFTWARE_TNL        1014
+#define TB_IDC_C_SAVE                   1015
+#define TB_IDC_C_DESCRIPTION            1016
+#define TB_IDC_C_LOAD                   1017
+#define TB_IDC_DSC_DRIVERS              1018
+#define TB_IDC_DSC_8_BITS               1019
+#define TB_IDC_DSC_16_BITS              1020
+#define TB_IDC_DSC_11025_HZ             1021
+#define TB_IDC_DSC_22050_HZ             1022
+#define TB_IDC_DSC_44100_HZ             1023
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
